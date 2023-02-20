44ff9e193327a2794effb83e421afd572075e353
Brandon <ncsbrandon@yahoo.com>
2023-02-20 12:24:19 -0500

    Update nebulaService.c

--- myWorkspace/mangOH/apps/nebulaService/nebulaService/nebulaService.c
+++ myWorkspace/mangOH/apps/nebulaService/nebulaService/nebulaService.c
@@ -1,7 +1,13 @@
 #include "legato.h"
 #include "interfaces.h"
 
+#include "/home/firstuser/nebula/myWorkspace/mangOH/build/yellow_wp77xx/api/83d1ed450fec70d0341448e59d96f374/client/dhubIO_interface.h"
+
+static const char Led0ResPath[] = "led/0";
+
 COMPONENT_INIT
 {
-    LE_INFO("Hello NEBULA! yeah!");
+    LE_INFO("Hello NEBULA 20Jan!");
+
+    LE_ASSERT_OK(dhubIO_CreateInput(Led0ResPath, DHUBIO_DATA_TYPE_BOOLEAN, ""));
 }
